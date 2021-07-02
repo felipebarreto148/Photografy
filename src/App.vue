caa61deeea74b07d7caf51f85474fa050fc1e4ce
felipe barreto <felipebarreto148@gmail.com>
2021-07-02 12:05:55 -0300

    Created header, carousel and gallery

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app flat>
       <Header />
     </v-app-bar>
     <v-main>
-      
+      <Hero />
+      <Gallery />
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Gallery from "./components/Gallery";
 
 export default {
   name: "App",
 
   components: {
     Header,
+    Hero,
+    Gallery,
   },
 
   data: () => ({
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,25 @@
 <template>
   <v-container>
-    Header
+    <v-toolbar flat>
+      <v-toolbar-title>Photography</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text> Home </v-btn>
+
+        <v-btn text> About </v-btn>
+
+        <v-btn text> Contact </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'Header',
+export default {
+  name: "Header",
 
-    data: () => ({
-     
-    }),
-  }
+  data: () => ({}),
+};
 </script>
