73c684966e1a6ae4ce61a7d96cae36b8fac30105
felipe barreto <felipebarreto148@gmail.com>
2021-07-04 10:23:21 -0300

    Created page not found

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,13 +1,20 @@
 <template lang="">
-    <v-content>
-        Not Found
-    </v-content>
+	<v-main>
+		<v-container>
+			<div class="block text-center notFound">
+				<h2>Page not found</h2>
+				<i class="fas fa-exclamation-triangle red--text"></i>
+				<p>
+					We're sorry, the page you requested could not be found. Please go back
+					to the homepage or contact us at admin@bibekshakya.com
+				</p>
+			</div>
+		</v-container>
+	</v-main>
 </template>
 <script>
 export default {
-    name: 'NotFound'
-}
+	name: 'NotFound',
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
